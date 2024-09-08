bd8a63fc1cfb9988d7f036adccbd8d20afeac1ae
User1 <431410@mail.ru>
2024-09-08 20:02:52 +0900

    create updateview

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import DispatchListView, DispatchCreateView
+from mailing.views import DispatchListView, DispatchCreateView, DispatchDetailView, DispatchUpdateView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', DispatchListView.as_view(), name='dispatch_list'),
     path('dispatch/create/', DispatchCreateView.as_view(), name='dispatch_create'),
+    path('dispatch/<int:pk>/', DispatchDetailView.as_view(), name='dispatch_detail'),
+    path('dispatch/edit/<int:pk>/', DispatchUpdateView.as_view(), name='dispatch_update'),
 ]
