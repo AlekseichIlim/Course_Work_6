cc8c9afb7de9ab5e4133a23ff6fc5fb500169d55
User1 <431410@mail.ru>
2024-09-08 14:42:47 +0900

    startapp users

--- users/models.py
+++ users/models.py
@@ -1,13 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from mailing.models import NULLABLE
-
-
-class User(AbstractUser):
-    username = None
-    email = models.EmailField(unique=True, verbose_name='почта')
-    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+# Create your models here.
