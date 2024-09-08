b11e3e6d1f6e28c9827bd1e7e958686fb9bd9fb8
User1 <431410@mail.ru>
2024-09-08 19:18:33 +0900

    create listview and createview

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,16 @@
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from mailing.forms import DispatchForm
+from mailing.models import Dispatch
+
+
+class DispatchListView(ListView):
+    model = Dispatch
+
+
+class DispatchCreateView(CreateView):
+    model = Dispatch
+    form_class = DispatchForm
+    success_url = reverse_lazy('mailing:dispatch_list')
