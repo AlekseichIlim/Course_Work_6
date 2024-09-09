e5327afeca0f627250dc714981a4c6dbc2531f7d
User1 <431410@mail.ru>
2024-09-09 12:56:04 +0900

    corrected

--- mailing/views.py
+++ mailing/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from mailing.forms import DispatchForm
-from mailing.models import Dispatch
+from mailing.models import Dispatch, Client
 
 
 class DispatchListView(ListView):
@@ -19,6 +20,12 @@ class DispatchCreateView(CreateView):
 class DispatchDetailView(DetailView):
     model = Dispatch
 
+    def get_context_data(self, **kwargs):
+        context = super(DispatchDetailView, self).get_context_data(**kwargs)
+        context['clients'] = Client.objects.filter(dispatch=self.object)
+        print(context['clients'])
+        return context
+
 
 class DispatchUpdateView(UpdateView):
     model = Dispatch
@@ -26,3 +33,8 @@ class DispatchUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('mailing:dispatch_detail', args=[self.kwargs.get('pk')])
+
+
+class DispatchDeleteView(DeleteView):
+    model = Dispatch
+    success_url = reverse_lazy('mailing:dispatch_list')
