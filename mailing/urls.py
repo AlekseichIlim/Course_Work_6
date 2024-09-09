62a94c81881949351037e245ea03f4b31f595958
User1 <431410@mail.ru>
2024-09-09 12:55:59 +0900

    corrected

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import DispatchListView, DispatchCreateView, DispatchDetailView, DispatchUpdateView
+from mailing.views import DispatchListView, DispatchCreateView, DispatchDetailView, DispatchUpdateView, \
+    DispatchDeleteView
 
 app_name = MailingConfig.name
 
@@ -10,4 +11,6 @@ urlpatterns = [
     path('dispatch/create/', DispatchCreateView.as_view(), name='dispatch_create'),
     path('dispatch/<int:pk>/', DispatchDetailView.as_view(), name='dispatch_detail'),
     path('dispatch/edit/<int:pk>/', DispatchUpdateView.as_view(), name='dispatch_update'),
+    path('dispatch/delete/<int:pk>/', DispatchDeleteView.as_view(), name='dispatch_delete'),
+
 ]
