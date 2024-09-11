42b8957cfba4328bcc5ac6f069f11b7eec2ccc1d
User1 <431410@mail.ru>
2024-09-11 19:05:42 +0900

    create class AttemptList

--- mailing/urls.py
+++ mailing/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from mailing.apps import MailingConfig
 from mailing.views import DispatchListView, DispatchCreateView, DispatchDetailView, DispatchUpdateView, \
-    DispatchDeleteView
+    DispatchDeleteView, AttemptsListView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', DispatchListView.as_view(), name='dispatch_list'),
+    path('dispatch/attempts/', AttemptsListView.as_view(), name='attempts_list'),
     path('dispatch/create/', DispatchCreateView.as_view(), name='dispatch_create'),
     path('dispatch/<int:pk>/', DispatchDetailView.as_view(), name='dispatch_detail'),
     path('dispatch/edit/<int:pk>/', DispatchUpdateView.as_view(), name='dispatch_update'),
