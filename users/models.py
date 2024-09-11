5247f37c4a74fce34d9e49869484c06921ffcdc4
User1 <431410@mail.ru>
2024-09-11 21:44:43 +0900

    corrected

--- users/models.py
+++ users/models.py
@@ -1,15 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from mailing.models import NULLABLE
-
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Email адрес')
-    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True)
 
-    token = models.CharField(max_length=100, verbose_name='Токен', **NULLABLE)
+    token = models.CharField(max_length=100, verbose_name='Токен', blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
