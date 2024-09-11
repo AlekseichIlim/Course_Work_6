e6464bbce20638fbea8923fd2ff3a584cc40ba3d
User1 <431410@mail.ru>
2024-09-11 19:05:37 +0900

    create class AttemptList

--- mailing/views.py
+++ mailing/views.py
@@ -1,14 +1,24 @@
+from urllib import request
+
 from django.conf import settings
+from django.forms import inlineformset_factory
 from django.urls import reverse_lazy, reverse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from mailing.forms import DispatchForm
-from mailing.models import Dispatch, Client
+from mailing.models import Dispatch, Client, Attempts
+from mailing.services import send_mailing
 
 
 class DispatchListView(ListView):
     model = Dispatch
+
+    def get_context_data(self, **kwargs):
+        context = super(DispatchListView, self).get_context_data(**kwargs)
+        mailings = Dispatch.objects.filter(status='завершена')
+        context['dispatch_completed'] = mailings
+        return context
 
 
 class DispatchCreateView(CreateView):
@@ -17,19 +27,20 @@ class DispatchCreateView(CreateView):
     success_url = reverse_lazy('mailing:dispatch_list')
 
 
+
 class DispatchDetailView(DetailView):
     model = Dispatch
 
     def get_context_data(self, **kwargs):
         context = super(DispatchDetailView, self).get_context_data(**kwargs)
         context['clients'] = Client.objects.filter(dispatch=self.object)
-        print(context['clients'])
         return context
 
 
 class DispatchUpdateView(UpdateView):
     model = Dispatch
     form_class = DispatchForm
+
 
     def get_success_url(self):
         return reverse('mailing:dispatch_detail', args=[self.kwargs.get('pk')])
@@ -38,3 +49,7 @@ class DispatchUpdateView(UpdateView):
 class DispatchDeleteView(DeleteView):
     model = Dispatch
     success_url = reverse_lazy('mailing:dispatch_list')
+
+
+class AttemptsListView(ListView):
+    model = Attempts
