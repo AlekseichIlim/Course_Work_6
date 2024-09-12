5318096825e09e0aff2ddf9be2475a1d48d7d3a7
User1 <431410@mail.ru>
2024-09-12 18:17:20 +0900

    corected

--- mailing/admin.py
+++ mailing/admin.py
@@ -7,21 +7,21 @@ from users.models import User
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('email', 'name', 'surname')
-    search_fields = ('email', 'surname')
+    search_fields = ('email', 'surname', 'owner')
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('title_message', )
     list_filter = ('title_message', 'topic_message')
-    search_fields = ('title_message', 'topic_message')
+    search_fields = ('title_message', 'topic_message', 'owner')
 
 
 @admin.register(Dispatch)
 class DispatchAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'status')
     list_filter = ('title', 'first_sent_date_time', 'periodicity', 'status')
-    search_fields = ('title', 'message', 'first_sent_date_time')
+    search_fields = ('title', 'message', 'first_sent_date_time', 'owner')
 
 
 @admin.register(Attempts)
@@ -35,4 +35,4 @@ class AttemptsAdmin(admin.ModelAdmin):
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'is_staff')
     list_filter = ('is_staff',)
-    search_fields = ('email',)
+    search_fields = ('email', )
