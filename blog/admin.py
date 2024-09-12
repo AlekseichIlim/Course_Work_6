883cf2e9b1efde16a4655dc6b4b1fac8dcd10929
User1 <431410@mail.ru>
2024-09-12 17:03:10 +0900

    register

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published', 'owner')
+    list_filter = ('title', 'is_published', 'owner')
+    search_fields = ('content', 'title')
