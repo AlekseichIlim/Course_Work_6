74db640bf8e290ca46fecf7fea9cf8df5bf3cc99
User1 <431410@mail.ru>
2024-09-12 15:41:28 +0900

    create urls

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import DispatchListView, DispatchCreateView, DispatchDetailView, DispatchUpdateView, DispatchDeleteView, AttemptsListView
+from mailing.views import DispatchListView, DispatchCreateView, DispatchDetailView, DispatchUpdateView, \
+    DispatchDeleteView, AttemptsListView, ClientListView, MessageListView, ClientDetailView, ClientUpdateView, \
+    ClientCreateView, ClientDeleteView, MessageCreateView, MessageDetailView, MessageUpdateView, MessageDeleteView
 
 app_name = MailingConfig.name
 
@@ -12,4 +14,14 @@ urlpatterns = [
     path('dispatch/<int:pk>/', DispatchDetailView.as_view(), name='dispatch_detail'),
     path('dispatch/edit/<int:pk>/', DispatchUpdateView.as_view(), name='dispatch_update'),
     path('dispatch/delete/<int:pk>/', DispatchDeleteView.as_view(), name='dispatch_delete'),
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('client/create/', ClientCreateView.as_view(), name='client_create'),
+    path('client/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('client/edit/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
+    path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
+    path('messages/', MessageListView.as_view(), name='message_list'),
+    path('message/create/', MessageCreateView.as_view(), name='message_create'),
+    path('message/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('message/edit/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+    path('message/delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
 ]
